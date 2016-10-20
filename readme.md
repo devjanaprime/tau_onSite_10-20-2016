c6bdc0c8489ce458939cdb1ddcc4e45da9588845
devjanaprime <dev@primeacademy.io>
2016-10-20 18:41:47 -0500

    tau on-site 10-20-2016

---
+++ readme.md
@@ -0,0 +1,9 @@
+Tau on site example 10-20-2016
+==============================
+
+[x] Create html file
+[x] src in js file
+[x] add a button to html
+[x] run a function in js when button is clicked
+[x] append text to p tag on DOM
+[x] commit to github repo
---
+++ scrupty.js
@@ -0,0 +1,7 @@
+console.log( 'hi from Scrupty' );
+
+var sayGoodbye = function(){
+  console.log( 'Goodbye, cruel world...' );
+  // display in the textOut p element on the dom
+  document.getElementById('textOut').innerHTML = 'Goodbye, cruel world!';
+}; // end sayGoodbye
